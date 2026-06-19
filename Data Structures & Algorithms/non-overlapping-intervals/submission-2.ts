e8d3f18bef4b0bea847874d177e196cb2b40675f
Jack Wang <19275035+JWPanda@users.noreply.github.com>
2026-06-19 13:45:08 -0400

    Add: non-overlapping-intervals - submission-2

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-2.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals: number[][]): number {
+        intervals.sort((a, b) => a[1] - b[1]);
+        let removed = 0;
+        let prevEnd = intervals[0][1];
+
+        for (let i = 1; i < intervals.length; i++) {
+            if (prevEnd > intervals[i][0]) {
+                removed++;
+            } else {
+                prevEnd = intervals[i][1];
+            }
+        }
+
+        return removed;
+    }
+}
