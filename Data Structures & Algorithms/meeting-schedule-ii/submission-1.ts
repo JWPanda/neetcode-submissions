9b048e270db0ae4a511044505459afa8cd605e6e
Jack Wang <19275035+JWPanda@users.noreply.github.com>
2026-06-25 13:11:10 -0400

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.ts
@@ -0,0 +1,48 @@
+/**
+ * Definition of Interval:
+ * class Interval  {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals: Interval[]): number { 
+        if (intervals.length === 0) return 0;
+        
+        let startStopArray = [];
+        let roomNeeded = 0;
+        let currOpenRooms = 0;
+
+
+        for (let i = 0; i < intervals.length; i++) {
+            startStopArray.push([intervals[i].start, 's'])
+            startStopArray.push([intervals[i].end, 'e'])
+        }
+        startStopArray.sort((a,b) => {
+            if (a[0] === b[0]) {
+                return b[1] === "e" ? 0 : -1;
+            }
+            return a[0] - b[0];
+        })
+        console.log(startStopArray)
+
+        for (let j = 0; j < startStopArray.length; j++) {
+            if (startStopArray[j][1] === 's') {
+                currOpenRooms += 1 ;
+            } else {
+                currOpenRooms -= 1 ;
+            }
+
+            roomNeeded = Math.max(currOpenRooms, roomNeeded)
+        }
+
+        return roomNeeded;
+    }
+}
